29a2c264a91e8472b80fc4c11a7b6dcb0f584dda
Phil Morle <phil@pollenizer.com>
2014-08-28 08:33:08 +0630

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'fb58b6a4cbaa51d6f4a922b6f0974dd7c18326400f6da5b2c745b7284b128befafdc87a81887867d9b8e5a8218bd1dd74cbfa97738f9269f5ad9ef31cfc1ade7'
